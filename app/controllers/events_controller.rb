3ffab1258348af2c9ef401e7e68565d89a86f382
Lindsey Derbyshire <lderbyshire@colgate.edu>
2017-11-14 19:55:56 -0500

    separate tables of classes/single events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events_recurring = Event.where(recurring: true)
+    @events_nonrecurring = Event.where(recurring: false)
   end
 
   def show
