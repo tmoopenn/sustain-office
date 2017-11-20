07f27f8ed0b7eed83bb26b7aad37dd628cab2c82
tmoopenn <tj-mp@hotmail.com>
2017-11-20 16:49:18 -0500

    enter participant rspec test for before filter

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe ParticipantsController, type: :controller do
+RSpec.describe ParticipantsController, type: [:request,:controller] do
+    describe "check filter" do
+        it "should redirect to events path with flash message when try to call participant controller with an event that does not exist" do
+        e = Event.new(:id=>12) 
+        get event_participants_path(e)  
+        expect(flash[:warning]).to match(/Event must exist to have participants/) 
+        expect(response).to redirect_to(events_path)
+        end
+    end
 
+    describe "create" do
+        it "should show a flash message on failure to create participant" do
+            
+        end
+    end
 end
