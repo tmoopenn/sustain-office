0ad1ff64d586a1879da4c867f25c390dc193daa8
nstraus <n_straus3@outlook.com>
2017-11-20 09:04:36 -0500

    migration typo

--- db/migrate/20171113215959_add_points_to_user.rb
+++ db/migrate/20171113215959_add_points_to_user.rb
@@ -1,5 +1,5 @@
 class AddPointsToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :tpoints, :integer,:default => 0
+    add_column :users, :points, :integer,:default => 0
   end
 end
