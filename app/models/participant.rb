e1d3d7390f75762873bb6fab96510b7396c725d1
Lindsey Derbyshire <lderbyshire@colgate.edu>
2017-12-03 14:56:02 -0500

    link participant model to occurrences

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,6 @@
 class Participant < ApplicationRecord
     belongs_to :user, :dependent => :destroy
-    belongs_to :event, :dependent => :destroy
+    belongs_to :occurrence, :dependent => :destroy
 
     validates :event_id, :presence => true
     validates :user_id, :presence => true
