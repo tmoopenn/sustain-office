cd92a383e5fa6aa8381673d871002875d61d9939
tmoopenn <tj-mp@hotmail.com>
2017-12-03 21:35:36 -0500

    basic implementation of submitting a summary completed and some minor changes

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,18 +2,34 @@ class AttendeesController < ApplicationController
   before_action :has_occurrence,:authenticate_user!
   protected
   def has_occurrence
-    #  unless @current_user
-    #      flash[:warning] = "You must be logged in to see participants or participate in an event"
-    #      redirect_to events_path(@event)
-    #  end
       unless (@occurrence = Occurrence.where(:id => params[:occurrence_id]).first)
           flash[:warning] = "Occurrence must exist to have registrees"
           redirect_to occurrences_path
       end
   end
-
+  
+  public
   def new
-    @attendee = Attendee.new
+  # byebug
+   # @attendee = @occurrence.attendees.build(:user_id => current_user.id)
+   @attendee = Attendee.new
+  end
+    
+  def create
+    @attendee = @occurrence.attendees.build(create_update_params)
+    @attendee.user_id = current_user.id
+    if @attendee.save
+        flash[:notice] = "Summary created for \'#{@occurrence.event.title}\'"
+        redirect_to user_path(current_user)
+    else
+        flash[:warning] = "Failed to create summary for \'#{@occurrence.event.title}\'"
+        redirect_to user_path(current_user) and return
+    end
+  end
+
+  private
+  def create_update_params
+    params.require(:attendee).permit(:summary)
   end
 
 end
