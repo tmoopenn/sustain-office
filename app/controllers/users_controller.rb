830bc8b2d54dd5bb42b26021c3aaf11305e1636d
nstraus <n_straus3@outlook.com>
2017-12-07 16:29:58 -0500

    admins can add other admins and users are required to mark their affiliation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,56 @@
 class UsersController < ApplicationController
+  skip_before_action :check_profile, only: [:edit, :update]
+  def show
+      id = params[:id]
+      @user = User.find(id)
+      @signed_in = current_user.id == @user.id
+  end
 
-    def show
-        id = params[:id]
-        @user = User.find(id)
+  def edit
+    id = params[:id]
+
+    if current_user.id != id.to_i
+      flash[:notice] = "You cannot edit some one else's profile!"
+      redirect_to action: "show", id: id
     end
+    @user = User.find(id)
+    @classifications = [["Student", "Student"], ["Factulty", "Factulty"], ["Staff", "Staff"]]
+  end
 
-    def leaderboard
-      @champions = User.rank1
-      @advocates = User.rank2
-      @associates = User.rank3
+  def leaderboard
+    @champions = User.rank1
+    @advocates = User.rank2
+    @associates = User.rank3
+  end
+
+  def update
+    id = params[:id]
+    @user = User.find(id)
+
+    if current_user.id == id.to_i || current_user.admin
+      @user.update create_update_params
+      if @user.save
+        flash[:notice] = "#{@user.name} has been updated"
+        redirect_to user_path @user
+      else
+        flash[:error] = "Error updating user"
+        redirect_to edit_user_path(@user)
+      end
     end
-
-    # def create
-    #     user = User.new(create_update_params)
-    #     if user.save
-    #         flash[:notice] = "New user \'#{user.name}\' created"
-    #         redirect_to events_path
-    #     else
-    #         flash[:error] = "Error in creating user"
-    #         redirect_to new_user_path
-    #     end
-    # end
-    #
-    # private
-    # def create_update_params
-    #   params.require(:user).permit(:name, :classification)
-    # end
+  end
+  # def create
+  #     user = User.new(create_update_params)
+  #     if user.save
+  #         flash[:notice] = "New user \'#{user.name}\' created"
+  #         redirect_to events_path
+  #     else
+  #         flash[:error] = "Error in creating user"
+  #         redirect_to new_user_path
+  #     end
+  # end
+  #
+  private
+  def create_update_params
+    params.require(:user).permit(:name, :classification, :admin)
+  end
 end
